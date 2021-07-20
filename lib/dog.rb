850e698b5611b81dedae9e65fe62f9e709f69040
Dan Beasley <dannyleebeasley@gmail.com>
2021-07-20 14:05:07 -0500

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    
+    def name=(name)
+        @this_dogs_name = name
+    end
+    def name
+        @this_dogs_name
+    end
 
 end
+
+snoopy = Dog.new
+snoopy.name = "Snoopy"
+puts snoopy.name
